8772c951542180c8392c8456e9002efdc114109c
Malek Boubakri <malekbouba@gmail.com>
2021-11-14 21:27:43 +0100

    add terms & privacy pages

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3999
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3999;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.listen(port, () => console.log(`app listening on port http://127.0.0.1:${port}!`))
+console.log(path.join(__dirname, "/public/privacy-policy.html"))
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+app.get("/terms", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/terms.html"))
+);
+app.get("/privacy-policy", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/privacy-policy.html"))
+);
+
+app.listen(port, () =>
+  console.log(`app listening on port http://127.0.0.1:${port}!`)
+);
