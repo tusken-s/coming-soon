5a4cbb7c5ee009474a840c8b5eabd632b21d5f78
Malek Boubakri <malekbouba@gmail.com>
2021-12-03 18:39:50 +0100

    overall re-styling

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const port = 3999;
 
 app.use(express.static("public"));
 
-console.log(path.join(__dirname, "/public/privacy-policy.html"))
-
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
@@ -16,6 +14,9 @@ app.get("/terms", (req, res) =>
 app.get("/privacy-policy", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/privacy-policy.html"))
 );
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/404.html"));
+});
 
 app.listen(port, () =>
   console.log(`app listening on port http://127.0.0.1:${port}!`)
