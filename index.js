b800389ceaad382268c10bb3592d324e4a7bddc9
Malek Boubakri <malekbouba@gmail.com>
2023-07-05 01:22:05 +0100

    file refact

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ const port = 3999;
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./pages/index.html"))
 );
 app.get("/terms", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/terms.html"))
+  res.sendFile(path.join(__dirname, "./pages/terms.html"))
 );
-// app.get("/freelance", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/public/freelance.html"))
-// );
 app.get("/privacy-policy", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/privacy-policy.html"))
+  res.sendFile(path.join(__dirname, "./pages/privacy-policy.html"))
 );
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/404.html"));
+  res.sendFile(path.join(__dirname, "./pages/404.html"));
 });
 
 app.listen(port, () =>
